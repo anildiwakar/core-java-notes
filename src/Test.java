c9f40ee5f93909529db250105f44e9555cb44c70
Anil <anildiwakar956@gmail.com>
2025-12-22 12:10:55 +0530

    First commit

--- src/Test.java
+++ src/Test.java
@@ -1,7 +1,28 @@
+import java.util.Scanner;
 
 public class Test {
 	public static void main(String[] args) {
-		System.out.println("Bye");
+		Scanner sc = new Scanner(System.in);
+		int day = sc.nextInt();
+		switch(day)
+		{
+			case 0: System.out.println("Sunday");
+			break;
+			case 1: System.out.println("Monday");
+			break;
+			case 2: System.out.println("Tuesday");
+			break;
+			case 3: System.out.println("Wednesday");
+			break;
+			case 4: System.out.println("Thurday");
+			break;
+			case 5: System.out.println("Friday");
+			break;
+			case 6: System.out.println("Saturday");
+			break;
+			default : System.out.println("Invalid day");
+		}
+		
 	}
 
 }
