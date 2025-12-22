d127b37a8ede71f7299d75a49083b8caa53a247c
Anil <anildiwakar956@gmail.com>
2025-12-22 14:35:17 +0530

    switch example program

--- src/SwitchExample.java
+++ src/SwitchExample.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Test {
+public class SwitchExample {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int day = sc.nextInt();
